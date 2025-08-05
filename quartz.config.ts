956eb674aba3947b2cb543150ebb91f9b0104373
madhavkauntia <madhav.kauntia@headout.com>
2025-08-05 12:37:49 +0530

    update baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jzhao.xyz",
+    baseUrl: "notes.madhavkauntia.com",
     ignorePatterns: ["private", "templates"],
     defaultDateType: "created",
     theme: {
